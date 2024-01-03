69208e00f67900edfc3c41acc46620c8d6583684
Chung Tu <tuthanhtrung2000@gmail.com>
2024-01-03 17:06:46 +0700

    feat: update dashboard, layout

--- app/dashboard.jsx
+++ app/dashboard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Image, Text, View } from 'react-native'
 import Background from '../components/Background'
 
 export default function Dashboard() {
   return (
     <Background classNameData={'w-screen h-screen p-5'}>
-      <Text className={'text-white'}>aaa</Text>
+      <View className={'flex items-center justify-center gap-3'}>
+        <View className={'bg-white w-20 h-20 rounded-full'} />
+        <Text className='text-white'>username</Text>
+      </View>
+      <View className='bg-black h-full w-full my-5'>
+        <Image source={require('../assets/dashboard.png')} className='mx-auto' />
+      </View>
     </Background>
   )
 }
